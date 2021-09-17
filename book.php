1412e8e21d08a0ba75ae67556ec0f8c06bea9999
∧° ┐ <we.are.pe.hu@gmail.com>
2021-09-17 22:02:51 +0900

    sample

--- book.php
+++ book.php
@@ -2,15 +2,15 @@
 function h($str) {
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
+
+$name = (string)filter_input(INPUT_POST, 'name');
 $title = (string)filter_input(INPUT_POST, 'title');
-$contents = (string)filter_input(INPUT_POST, 'contents');
-$tag = (string)filter_input(INPUT_POST, 'tag');
-$label = (string)filter_input(INPUT_POST, 'label');
+$link = (string)filter_input(INPUT_POST, 'link');
 
 $fp = fopen('book.csv', 'a+b');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     flock($fp, LOCK_EX);
-    fputcsv($fp, [$title, $contents, $tag, $label,]);
+    fputcsv($fp, [$name, $title, $link]);
     rewind($fp);
 }
 
@@ -27,69 +27,39 @@ fclose($fp);
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title> 自分にまつわる事柄のリストページ </title>
+<title> 📝  List of The Website of Listing somethings </title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="org.js"></script>
 <script type="text/javascript">
 </script>
 <link rel="stylesheet" href="book.css"/>
 <style type="text/css">
-.list #done {
-  zoom:1.5;
-  padding:1rem 1.25rem;
-}
-.list li span {
-  animation:2s ease-in infinite fontmotion;
+#contents {
+  margin:5vw 0 0;
 }
 </style>
 </head>
 <body>
 
-<form id="org">
-<div class="search-box tag">
-<ul>
-<li>
-<input type="radio" name="tag" value="bookcover" id="bookcover">
-<label for="bookcover" class="label">開いた本のようなページ</label></li>
-<li>
-<input type="radio" name="tag" value="book" id="book">
-<label for="book" class="label">本を読むようにテキストが表示される挨拶ページ</label></li>
-<li>
-<input type="radio" name="tag" value="list" id="list">
-<label for="list" class="label">自分にまつわる事柄のリストページ</label></li>
-</ul>
-</div>
-<div class="search-box status">
-<ul>
-<li>
-<input type="radio" name="label" value="a" id="a">
-<label for="a" class="label">使用例</label></li>
-<li>
-<input type="radio" name="label" value="b" id="b">
-<label for="b" class="label">応用編</label></li>
-</ul>
-</div>
-<div class="reset">
-<input type="reset" name="reset" value="RESET" class="reset-button">
-</div>
-</form>
-
-<ul class="list">
+<div id="contents">
+<p class="writing">📝 List of The Website of Listing somethings</p>
+<ul id="list">
 <?php if (!empty($rows)): ?>
 <?php foreach ($rows as $row): ?>
-<li id="<?=h($row[3])?>" class="list_item list_toggle" data-tag="<?=h($row[2])?>" data-label="<?=h($row[3])?>">
-<span><?=h($row[0])?></span>
-<p><?=h($row[1])?></p>
-<a href="<?=h($row[1])?>" target="_blank" rel="noopener noreferrer"></a>
+<li class="item">
+<p class="name"><?=h($row[0])?></p>
+<span class="title"><?=h($row[1])?></span>
+<a class="link" href="<?=h($row[2])?>" target="_blank" rel="noopener noreferrer"></a>
 </li>
 <?php endforeach; ?>
 <?php else: ?>
-<li id="<?=h($row[3])?>" class="list_item list_toggle" data-tag="<?=h($row[2])?>" data-label="<?=h($row[3])?>">
-<span>Title</span>
-<p>contents</p>
-<a href="<?=h($row[1])?>" target="_blank" rel="noopener noreferrer"></a>
+<li class="item">
+<p class="name">Name</p>
+<span class="title">Title</span>
+<a class="link" href="" target="_blank" rel="noopener noreferrer"></a>
 </li>
 <?php endif; ?>
 </ul>
+</div>
 </body>
 </html>
--- submit.php
+++ submit.php
@@ -1,77 +1,21 @@
-<?php
-function h($str) {
-    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-}
-$title = (string)filter_input(INPUT_POST, 'title');
-$contents = (string)filter_input(INPUT_POST, 'contents');
-$tag = (string)filter_input(INPUT_POST, 'tag');
-$label = (string)filter_input(INPUT_POST, 'label');
-
-$fp = fopen('book.csv', 'a+b');
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    flock($fp, LOCK_EX);
-    fputcsv($fp, [$title, $contents, $tag, $label,]);
-    rewind($fp);
-}
-
-flock($fp, LOCK_SH);
-while ($row = fgetcsv($fp)) {
-    $rows[] = $row;
-}
-flock($fp, LOCK_UN);
-fclose($fp);
-
-?>
-<html lang="ja">
+<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title> Submit | 自分にまつわる事柄のリストページ </title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script type="text/javascript">
-$(function(){
-$("#").load("");
-})
-</script>
+<title>✍ Let's Creat a Lists of the Things about You</title>
 <link rel="stylesheet" href="book.css"/>
 <style type="text/css">
 </style>
 </head>
 <body>
-<div id="header">
-<a href="book.php" target="_parent">自分にまつわる事柄のリストページ </a>
-<a href="submit.php" target="_parent">Submit</a>
-</div>
-<form action="complete.php" id="org" method="post" target="_parent">
-<p><input type="text" name="title" placeholder="title" required></p>
-<div class="search-box tag">
-<ul>
-<li>
-<input type="radio" name="tag" value="bookcover" id="bookcover">
-<label for="bookcover" class="label">開いた本のようなページ</label></li>
-<li>
-<input type="radio" name="tag" value="book" id="book">
-<label for="book" class="label">本を読むようにテキストが表示される挨拶ページ</label></li>
-<li>
-<input type="radio" name="tag" value="list" id="list">
-<label for="list" class="label">自分にまつわる事柄のリストページ</label></li>
-</ul>
-</div>
-<div class="search-box status">
-<ul>
-<li>
-<input type="radio" name="label" value="a" id="a">
-<label for="a" class="label">使用例</label></li>
-<li>
-<input type="radio" name="label" value="b" id="b">
-<label for="b" class="label">応用編</label></li>
-</ul>
-<p><textarea name="contents" placeholder="about this" required></textarea></p>
-</div>
-<div class="reset">
-<button type="submit">Submit | 投稿する</button>
-</div>
+<div id="contents">
+<p class="writing">✍ 投稿フォーム</p>
+<section id="form">
+<form action="complete.php" method="post">
+<input type="text" name="name" placeholder="タイトル" required>
+<input type="text" name="title" placeholder="URL" required>
+<input type="url" name="link" placeholder="URL" required>
+<button type="submit">投稿 POST</button>
 </form>
+</section>
+</div>
 </body>
-</html>
